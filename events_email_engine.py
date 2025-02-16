96f7c875bd60e8af16af48af9b4bdc81a61a72e8
Raja Shravan <raja@revv.xyz>
2025-02-15 22:47:25 -0500

    adds birthday support

--- events_email_engine.py
+++ events_email_engine.py
@@ -3,45 +3,55 @@ from collections import defaultdict
 from random import randrange
 
 GRID_SIZE = 10
+NUM_DAYS = 365
+BIRTHDAY_WINDOW = 7
 
 class Event:
-	def __init__(self, categories, lat=None, lon=None):
+	def __init__(self, categories, lat=None, lon=None, day=None):
 		self.id = str(uuid4())[:5]
 		self.categories = {}
 		for e in categories:
 			self.categories[e] = True
 
-		if not lat:
+		if lat is None:
 			lat = randrange(GRID_SIZE)
-		if not lon:
+		if lon is None:
 			lon = randrange(GRID_SIZE)
 		
 		self.lat = lat
 		self.lon = lon
+
+		if day is None:
+			day = randrange(NUM_DAYS)
+		self.day = day
 		
 	def add_category(self, category):
 		self.categories[category] = True
 	
 	def __str__(self):
-		return f"Event: ID {str(self.id)[:5]} and cats: {', '.join(self.categories.keys())}. Coords: {self.lat}, {self.lon}"
+		return f"Event: ID {str(self.id)[:5]} and cats: {', '.join(self.categories.keys())}. Coords: {self.lat}, {self.lon}. Time: {self.day}"
 
 class User:
-	def __init__(self, fav_categories, lat=None, lon=None):
+	def __init__(self, fav_categories, lat=None, lon=None, birthday=None):
 		self.id = str(uuid4())[:5]
 		self.fav_categories = {}
 		for cat in fav_categories:
 			self.fav_categories[cat] = True
 	 
 		if lat is None:
-			lat = randrange(10)
+			lat = randrange(GRID_SIZE)
 		if lon is None:
-			lon = randrange(10)
+			lon = randrange(GRID_SIZE)
 		
 		self.lat = lat
 		self.lon = lon
+
+		if birthday is None:
+			birthday = randrange(NUM_DAYS)
+		self.birthday = birthday
 	
 	def __str__(self):
-		return f"User: ID {str(self.id)[:5]} and cats: {', '.join(self.fav_categories.keys())}. Coords: {self.lat}, {self.lon}"
+		return f"User: ID {str(self.id)[:5]} and cats: {', '.join(self.fav_categories.keys())}. Coords: {self.lat}, {self.lon}. Bday: {self.birthday}"
 	
 
 class StubHub:
@@ -61,6 +71,7 @@ class StubHub:
 		self.process()
 
 		self.make_event_location_grid()
+		self.make_calendar()
 	
 	def make_event_location_grid(self):
 		self.event_location_grid = defaultdict(lambda: defaultdict(set))
@@ -92,6 +103,30 @@ class StubHub:
 		print("Nearby events for user " + user.id)
 		print(nearby_events)
 
+	def find_events_close_user_birthday(self, user):
+		window = BIRTHDAY_WINDOW
+		half_window = window // 2
+
+		days_to_check = []
+		for i in range(window):
+			day = (user.birthday - half_window + i) % NUM_DAYS
+			days_to_check.append(day)
+
+		events_close_to_birthday = []
+		for day in days_to_check:
+			events_close_to_birthday.extend(self.calendar[day])
+		
+		print("Events close to bday for user " + user.id)
+		print(events_close_to_birthday)
+
+	def make_calendar(self):
+		self.calendar = defaultdict(set)
+
+		for event in self.events.values():
+			self.add_event_to_calendar(event)
+
+	def add_event_to_calendar(self, event):
+		self.calendar[event.day].add(event.id)
 	
 	def process(self):
 		# processes all recs from 0
@@ -127,6 +162,9 @@ class StubHub:
 
 		# add to grid
 		self.add_event_to_grid(event)
+
+		# add to cal
+		self.add_event_to_calendar(event)
 		
 		# update recs
 		for event_cat in event.categories:
@@ -181,14 +219,12 @@ class StubHub:
 
 
 # main
-# # TODO: location, birthday
-
-origin_user = User([], 0, 0)
+origin_user = User([], 0, 0, 363)
 
 engine = StubHub(
 	[
-	 Event(['rock', 'pop']),
-	 Event(['rap'], 1, 1),
+	 Event(['rock', 'pop'], 4, 5, 360),
+	 Event(['rap'], 1, 1, 1),
 	],
 	[
 	 User(['rock', 'folk']),
@@ -214,3 +250,4 @@ engine.send_messages()
 engine.print_event_location_grid()
 
 engine.find_nearby_events_for_user(origin_user)
+engine.find_events_close_user_birthday(origin_user)
